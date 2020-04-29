316c38c7e748472c7eb048d35b12ab068090e805
Mingwei Deng <34043558+DDDennnisss@users.noreply.github.com>
2020-04-29 15:56:01 -0400

    finished basic pig game

--- DOM-pig-game/starter/app.js
+++ DOM-pig-game/starter/app.js
@@ -11,79 +11,85 @@ GAME RULES:
 
 var score, roundScore, activePlayer;
 
-initial()
-document.querySelector('.btn-roll').addEventListener('click', () => {
-
+initial();
+document.querySelector(".btn-roll").addEventListener("click", () => {
+  if (gamePlaying) {
     var dice = Math.floor(Math.random() * 6) + 1;
 
-    var diceDom = document.querySelector('.dice');
+    var diceDom = document.querySelector(".dice");
 
-    diceDom.style.display = 'block';
-    diceDom.src = 'dice-' + dice + '.png'
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + dice + ".png";
 
     if (dice !== 1) {
-        roundScore += dice;
-        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+      roundScore += dice;
+      document.querySelector(
+        "#current-" + activePlayer
+      ).textContent = roundScore;
+    } else {
+      nextPlayer();
     }
-    else {
-        nextPlayer();
-    }
-
+  }
 });
 
-document.querySelector('.btn-hold').addEventListener('click', () => {
-
+document.querySelector(".btn-hold").addEventListener("click", () => {
+  if (gamePlaying) {
     score[activePlayer] += roundScore;
-    document.querySelector('#score-' + activePlayer).textContent = score[activePlayer];
+    document.querySelector("#score-" + activePlayer).textContent =
+      score[activePlayer];
 
     if (score[activePlayer] >= 100) {
-        document.querySelector('#name-' + activePlayer).textContent = 'winnter';
-        document.querySelector('.dice').style.display = 'none';
-        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner')
-        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active')
+      document.querySelector("#name-" + activePlayer).textContent = "winnter";
+      document.querySelector(".dice").style.display = "none";
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.add("winner");
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.remove("active");
+      gamePlaying = false;
+    } else {
+      nextPlayer();
     }
-    else {
-        nextPlayer()
-    }
+  }
 });
 
-document.querySelector('.btn-new').addEventListener('click', initial);
+document.querySelector(".btn-new").addEventListener("click", initial);
 
 function nextPlayer() {
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScore = 0;
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
 
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
+  // document.querySelector('.player-0-panel').classList.remove('active');
+  // document.querySelector('.player-1-panel').classList.add('active');
 
-    // document.querySelector('.player-0-panel').classList.remove('active');
-    // document.querySelector('.player-1-panel').classList.add('active');
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
 
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-
-    document.querySelector('.dice').style.display = 'none';
-
+  document.querySelector(".dice").style.display = "none";
 }
 
 function initial() {
-    score = [0, 0];
-    roundScore = 0;
-    activePlayer = 0;
+  score = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+  gamePlaying = true;
 
-    document.querySelector('.dice').style.display = 'none';
+  document.querySelector(".dice").style.display = "none";
 
-    document.getElementById('score-0').textContent = 0;
-    document.getElementById('score-1').textContent = 0;
-    document.getElementById('current-0').textContent = 0;
-    document.getElementById('current-1').textContent = 0;
+  document.getElementById("score-0").textContent = 0;
+  document.getElementById("score-1").textContent = 0;
+  document.getElementById("current-0").textContent = 0;
+  document.getElementById("current-1").textContent = 0;
 
-    document.getElementById('name-0').textContent = 'Player 1';
-    document.getElementById('name-1').textContent = 'Player 2';
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
 
-    document.querySelector('.player-0-panel').classList.remove('winner')
-    document.querySelector('.player-1-panel').classList.remove('winner')
-    document.querySelector('.player-0-panel').classList.remove('active')
-    document.querySelector('.player-1-panel').classList.remove('active')
-    document.querySelector('.player-0-panel').classList.add('active')
-};
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+  document.querySelector(".player-0-panel").classList.add("active");
+}
--- basic-learning/Day1/Assignment1.js
+++ basic-learning/Day1/Assignment1.js
@@ -1,24 +1,24 @@
 var john = {
     fullName: "John Smith",
-    bills:[124,48,268,180,42],
-    calcTips: function(){
-        this.tips=[];
-        this.finalValue=[];
+    bills: [124, 48, 268, 180, 42],
+    calcTips: function () {
+        this.tips = [];
+        this.finalValue = [];
 
-        for (var i=0; i<this.bills.length; i++){
+        for (var i = 0; i < this.bills.length; i++) {
             var percentage;
             var bill = this.bills[i];
 
-            if(bill<50){
-                percentage=0.2;
+            if (bill < 50) {
+                percentage = 0.2;
             }
-            else if (bill>=50 && bill< 200){
-                percentage=0.15;
+            else if (bill >= 50 && bill < 200) {
+                percentage = 0.15;
             }
-            else{
-                percentage=0.1;
+            else {
+                percentage = 0.1;
             }
-            this.tips[i] = bill*percentage;
+            this.tips[i] = bill * percentage;
             this.finalValue = bill + this.tips[i];
         }
 
@@ -26,38 +26,38 @@ var john = {
 }
 var mark = {
     fullName: "Mark Smith",
-    bills:[77,475,110,45],
-    calcTips: function(){
-        this.tips=[];
-        this.finalValue=[];
+    bills: [77, 475, 110, 45],
+    calcTips: function () {
+        this.tips = [];
+        this.finalValue = [];
 
-        for (var i=0; i<this.bills.length; i++){
+        for (var i = 0; i < this.bills.length; i++) {
             var percentage;
             var bill = this.bills[i];
 
-            if(bill<50){
-                percentage=0.2;
+            if (bill < 50) {
+                percentage = 0.2;
             }
-            else if (bill>=50 && bill< 200){
-                percentage=0.1;
+            else if (bill >= 50 && bill < 200) {
+                percentage = 0.1;
             }
-            else{
-                percentage=0.25;
+            else {
+                percentage = 0.25;
             }
-            this.tips[i] = bill*percentage;
+            this.tips[i] = bill * percentage;
             this.finalValue = bill + this.tips[i];
         }
 
     }
 }
 
-function calcAverageTips(tips){
+function calcAverageTips(tips) {
     var sum = 0;
 
-    for (var i=0; i<tips.length;i++){
-        sum +=tips[i]
+    for (var i = 0; i < tips.length; i++) {
+        sum += tips[i]
     }
-    var aveSum = sum/tips.length;
+    var aveSum = sum / tips.length;
     return aveSum;
 }
 
