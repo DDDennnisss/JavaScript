dee6286ea3caf13b75524ca31ef0c2af4090d94c
Mingwei Deng <34043558+DDDennnisss@users.noreply.github.com>
2020-05-04 12:24:06 -0400

    finish the advance challenge 2

--- basic-learning/Day4/challenge2.js
+++ basic-learning/Day4/challenge2.js
@@ -1,35 +1,69 @@
-function Question(question, answers, correct) {
-    this.question = question;
-    this.answers = answers;
-    this.correct = correct;
-}
-
-Question.prototype.displayQuestion = function () {
-    console.log(this.question);
-
-    for (var i = 0; i < this.answers.length; i++) {
-        console.log(i+': '+this.answers[i])
+(function () {
+    function Question(question, answers, correct) {
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
     }
-}
 
-Question.prototype.checkAnswer = function(ans){
-    if(ans === this.correct){
-        console.log('Correct answer!')
+    function updateScore() {
+        var score = 0;
+        return function (correct) {
+            if (correct) {
+                score++;
+            }
+            return score;
+        }
     }
-    else{
-        console.log('Wrong answer. try again!')
+
+    var keepScore = updateScore();
+
+    Question.prototype.displaySocre = function (score) {
+        console.log("Your current socre is: " + score);
+        console.log("-------------------------------------------");
     }
-}
 
-var q1 = new Question("is Javascript the coolest?", ['Yes', 'No'], 0);
-var q2 = new Question("Whats the name of this courses' teacher?", ['Dennis', 'John', 'Ash'], 2);
-var q3 = new Question("Whats does the best decrible coding?", ['boring', 'hard', 'fun', 'interesting'], 3);
+    Question.prototype.displayQuestion = function () {
+        console.log(this.question);
 
-var questions = [q1, q2, q3]
-var n = Math.floor(Math.random() * questions.length);
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' + this.answers[i])
+        }
+    }
 
-questions[n].displayQuestion();
+    Question.prototype.checkAnswer = function (ans, callback) {
+        var score;
 
-var answer = parseInt(prompt('Please select the correct answer.'));
+        if (ans === this.correct) {
+            console.log('Correct answer!');
+            score = callback(true);
+        }
+        else {
+            console.log('Wrong answer. try again!');
+            score = callback(false);
+        }
 
-questions[n].checkAnswer(answer);
+        this.displaySocre(score);
+    }
+
+    function nextQuestion() {
+        var q1 = new Question("is Javascript the coolest?", ['Yes', 'No'], 0);
+        var q2 = new Question("Whats the name of this courses' teacher?", ['Dennis', 'John', 'Ash'], 2);
+        var q3 = new Question("Whats does the best decrible coding?", ['boring', 'hard', 'fun', 'interesting'], 3);
+
+        var questions = [q1, q2, q3]
+        var n = Math.floor(Math.random() * questions.length);
+
+        questions[n].displayQuestion();
+
+        var answer = prompt('Please select the correct answer.');
+
+        if (answer !== 'exit') {
+            questions[n].checkAnswer(parseInt(answer), keepScore);
+            nextQuestion();
+        }
+    }
+
+    nextQuestion();
+})();
+
+
