9760746af7816fa7af496e8807d13cd1a6795503
Mingwei Deng <34043558+DDDennnisss@users.noreply.github.com>
2020-05-05 13:42:46 -0400

    uodate UI controller

--- basic-learning/Day6/budgety/starter/app.js
+++ basic-learning/Day6/budgety/starter/app.js
@@ -1,27 +1,53 @@
+// BUDGET CONTROLLER
 var budgetController = (function () {
-    var x = 5;
-    var add = function (a) {
-        return a + x;
-    }
-    return {
-        publicTest: function (b) {
-            return add(b)
-        }
-    }
+
 
 })();
 
+// UI CONTROLLER
 var UIController = (function () {
 
-})();
-
-var controller = (function (budgetCtrl, UICtrl) {
-
-    var z = budgetCtrl.publicTest(5)
+    var DomString = {
+        getType: '.add__type',
+        getDescription: '.add__description',
+        getValue: '.add__value',
+        getBtn: '.add__btn'
+    }
 
     return {
-        anotherTest: function () {
-            console.log(z)
+        getInput: function () {
+            return {
+                type: document.querySelector(DomString.getType).value,
+                description: document.querySelector(DomString.getDescription).value,
+                value: document.querySelector(DomString.getValue).value
+            };
+        },
+
+        getDomString: function () {
+            return DomString;
         }
     }
+})();
+
+
+//GLOBAL APP CONTROLLER
+var controller = (function (budgetCtrl, UICtrl) {
+
+    var DOM = UICtrl.getDomString();
+
+    var ctrlAddItem = function () {
+        var input = UICtrl.getInput;
+        console.log(input)
+    }
+
+    document.querySelector(DOM.getBtn).addEventListener('click', ctrlAddItem)
+
+    document.addEventListener('keypress', function (e) {
+
+        if (e.keyCode === 13 || e.which === 13) {
+            ctrlAddItem()
+        }
+
+    })
+
 })(budgetController, UIController)
