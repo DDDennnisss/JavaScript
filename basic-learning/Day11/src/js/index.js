48dab57df0eac2c56b2253027e5fd5c9c2256f82
Mingwei Deng <34043558+DDDennnisss@users.noreply.github.com>
2020-05-21 09:56:32 -0400

    babel setup done

--- basic-learning/Day11/src/js/index.js
+++ basic-learning/Day11/src/js/index.js
@@ -1,4 +1,4 @@
 // Global app controllerimport num from "./test"
 import x from "./test";
-
-console.log(`I import ${x} from another module.`)
+const num = 23;
+console.log(`I import ${x} from another module. varible x is ${num}`)
